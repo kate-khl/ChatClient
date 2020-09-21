1dc4b6e085fdec79f88a7f380ce0996c908d1f2b
Kate Khl <kate-khlystunova@yandex.ru>
2020-09-21 16:38:39 +0300

    create scanner

--- src/main/java/org/khl/chat/ChatClientApplication.java
+++ src/main/java/org/khl/chat/ChatClientApplication.java
@@ -1,13 +1,27 @@
 package org.khl.chat;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class ChatClientApplication {
+public class ChatClientApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatClientApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws IOException {
+//    	System.out.println("dfbg");
+//    	System.in.read();
+//    	System.out.println("fgsfbv");
+    	
+    	Scanner console = new Scanner(System.in);
+    	String name = console.nextLine();
+    	System.out.println(name);
+    	
+    }
 }
---
+++ src/main/java/org/khl/chat/CmdScanner.java
@@ -0,0 +1,10 @@
+package org.khl.chat;
+
+public class CmdScanner {
+	
+	public void parser(String inputStr) {
+
+		
+
+	}
+}
